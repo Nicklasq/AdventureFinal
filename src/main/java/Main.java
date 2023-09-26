38218986f80e2379e3ef6969538b4849a39a3969
nicklasq <nicklascao@Nicklass-MacBook-Pro.local>
2023-09-26 11:08:00 +0200

    Changes by Sophus Ingi

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,12 @@
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        UserInterface ui = new UserInterface();
+        ui.startProgram();
+        //Scanner input =
+    }
+
+    //GameRoom myGameRoom = new GameRoom();
+        //myGameRoom.buildRoom();
 }
